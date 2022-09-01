874d6d7f61a4d7e67431eeaa7f7801c71ef5fefb
Alexandre <aleaxe7@gmail.com>
2022-09-01 19:21:07 -0300

    Remove unnecessary folder

---
+++ odin-foundations/rock-paper-scissors/script.js
@@ -0,0 +1,14 @@
+
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const random = Math.floor(Math.random() * choices.length);
+
+    return choices[random]
+}
+
+function playSingleRound(playerSelection, computerSelection) {
+    if 
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
