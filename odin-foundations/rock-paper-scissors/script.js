22a1d3e90d011fc3cbbb039ab6396b4ace7f6076
Alexandre <aleaxe7@gmail.com>
2022-09-01 20:01:48 -0300

    Add game main function to get player choice and compute result

--- odin-foundations/rock-paper-scissors/script.js
+++ odin-foundations/rock-paper-scissors/script.js
@@ -1,14 +1,38 @@
 
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
-    const random = Math.floor(Math.random() * choices.length);
+    let random = Math.floor(Math.random() * choices.length);
 
     return choices[random]
 }
 
 function playSingleRound(playerSelection, computerSelection) {
-    if 
+    if (String(playerSelection).toLowerCase() === String(computerSelection).toLowerCase()) {
+        return `It's a tie. Your choice was ${playerSelection} and the computer choice was ${computerSelection} too.`
+    } else if (playerSelection.toLowerCase() === "rock"){
+        if (computerSelection.toLowerCase() === "scissors"){
+            return `You won! ${playerSelection} beats ${computerSelection}.`            
+        } else {
+            return `You lost! ${computerSelection} beats ${playerSelection}.`
+        }
+    } else if (playerSelection.toLowerCase() === "paper"){
+        if (computerSelection.toLowerCase() === "rock"){
+            return `You won! ${playerSelection} beats ${computerSelection}.`            
+        } else {
+            return `You lost! ${computerSelection} beats ${playerSelection}.`
+        }
+    } else if (playerSelection.toLowerCase() === "scissors"){
+        if (computerSelection.toLowerCase() === "paper"){
+            return `You won! ${playerSelection} beats ${computerSelection}.`            
+        } else {
+            return `You lost! ${computerSelection} beats ${playerSelection}.`
+        }
+    } else {
+        return `You entered a invalid input: ${player}.`
+    }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+const player = prompt("Please enter your choice [Rock, Paper or Scissors]:");
+const computer = getComputerChoice()
+
+console.log(playSingleRound(player, computer))
